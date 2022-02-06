1dc673398861f16066fc98f2b576919abd48b110
Arsenii <arsenii.mingazov@belstu.by>
2022-02-06 20:05:55 +0300

    Initialized the initial route structure

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
 require('dotenv').config();
 
 // app
@@ -22,12 +23,9 @@ app.use(morgan('dev'));
 app.use(bodyParser.json({ limit: '2mb' }));
 app.use(cors());
 
-// route
-app.get('/api', (req, res) => {
-    res.json({
-        data: 'Hey you hit node API',
-    });
-});
+fs.readdirSync('./routes').map((r) =>
+    app.use('/api', require('./routes/' + r))
+);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log('Server is running'));
