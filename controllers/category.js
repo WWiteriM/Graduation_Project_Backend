3258b7ffb90290448d5591e1635a1ecc407194c2
Arsenii <arsenii.mingazov@belstu.by>
2022-03-07 14:40:24 +0300

    Created category controller

--- controllers/category.js
+++ controllers/category.js
@@ -12,17 +12,33 @@ exports.create = async (req, res) => {
 };
 
 exports.list = async (req, res) => {
-
+    await res.json(await Category.find({}).sort({ createdAt: -1 }).exec());
 };
 
 exports.read = async (req, res) => {
-
+    const category = await Category.findOne({ slug: req.params.slug }).exec();
+    await res.json(category);
 };
 
 exports.update = async (req, res) => {
-
+    const { name } = req.body;
+    try {
+        const updated = await Category.findOneAndUpdate(
+            { slug: req.params.slug },
+            { name, slug: slugify(name) },
+            { new: true }
+        );
+        await res.json(updated);
+    } catch (err) {
+        res.status(400).send('Update category failed');
+    }
 };
 
 exports.remove = async (req, res) => {
-
+    try {
+        const deleted = await Category.findOneAndDelete({ slug: req.params.slug });
+        await res.json(deleted);
+    } catch (err) {
+        res.status(400).send('Delete category failed');
+    }
 };
