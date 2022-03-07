8db6aabe66220c872ee6994567e496bd5e9e779c
Arsenii <arsenii.mingazov@belstu.by>
2022-03-07 13:42:03 +0300

    Created admin route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { authCheck } = require('../middlewares/auth');
+const { authCheck, adminCheck } = require('../middlewares/auth');
 const { createOrUpdateUser, currentUser } = require('../controllers/auth');
 
 const router = express.Router();
 
 router.post('/create-or-update-user', authCheck, createOrUpdateUser);
 router.post('/current-user', authCheck, currentUser);
+router.post('/current-admin', authCheck, adminCheck, currentUser);
 
 module.exports = router;
