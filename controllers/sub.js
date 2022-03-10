243bc5371a7e8001647926935d4dac75d4f85f8d
Arsenii <arsenii.mingazov@belstu.by>
2022-03-10 20:06:03 +0300

    Created sub category API

---
+++ controllers/sub.js
@@ -0,0 +1,44 @@
+const Sub = require('../models/sub');
+const slugify = require('slugify');
+
+exports.create = async (req, res) => {
+    try {
+        const { name, parent } = req.body;
+        await res.json(await new Sub({ name, parent, slug: slugify(name) }).save());
+
+    } catch (err) {
+        res.status(400).send('Create sub-category failed');
+    }
+};
+
+exports.list = async (req, res) => {
+    await res.json(await Sub.find({}).sort({ createdAt: -1 }).exec());
+};
+
+exports.read = async (req, res) => {
+    const sub = await Sub.findOne({ slug: req.params.slug }).exec();
+    await res.json(sub);
+};
+
+exports.update = async (req, res) => {
+    const { name, parent } = req.body;
+    try {
+        const updated = await Sub.findOneAndUpdate(
+            { slug: req.params.slug },
+            { name, parent, slug: slugify(name) },
+            { new: true }
+        );
+        await res.json(updated);
+    } catch (err) {
+        res.status(400).send('Update sub-category failed');
+    }
+};
+
+exports.remove = async (req, res) => {
+    try {
+        const deleted = await Sub.findOneAndDelete({ slug: req.params.slug });
+        await res.json(deleted);
+    } catch (err) {
+        res.status(400).send('Delete sub-category failed');
+    }
+};
