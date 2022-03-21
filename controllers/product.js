bb907d722a0a35fb69dfcefd38bbcc11ae45ed8f
Arsenii <arsenii.mingazov@belstu.by>
2022-03-21 18:40:10 +0300

    Implemented update product API

--- controllers/product.js
+++ controllers/product.js
@@ -10,7 +10,7 @@ exports.create = async (req, res) => {
         // res.status(400).send('Create product failed');
        await res.status(400).json({
             err: err.message,
-        });
+       });
     }
 };
 
@@ -39,4 +39,22 @@ exports.read = async (req, res) => {
         .populate('subs')
         .exec();
     await res.json(product);
+};
+
+exports.update = async (req, res) => {
+    try {
+        if (req.body.title) {
+            req.body.slug = slugify(req.body.title);
+        }
+        const updated = await Product.findOneAndUpdate(
+            { slug: req.params.slug },
+            req.body,
+            { new: true }
+        ).exec();
+        await res.json(updated);
+    } catch (err) {
+        await res.status(400).json({
+            err: err.message,
+        });
+    }
 };
--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { authCheck, adminCheck } = require('../middlewares/auth');
-const { create, listAll, remove, read } = require('../controllers/product');
+const { create, listAll, remove, read, update } = require('../controllers/product');
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.post('/product', authCheck, adminCheck, create);
 router.get('/products/:count', listAll);
 router.delete('/product/:slug', authCheck, adminCheck, remove);
 router.get('/product/:slug', read);
+router.put('/product/:slug', authCheck, adminCheck, update);
 
 module.exports = router;
