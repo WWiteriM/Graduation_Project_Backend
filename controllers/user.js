e5d94924554d1b1e6279420ee23bfbfea360339c
Arsenii <arsenii.mingazov@belstu.by>
2022-04-03 17:03:20 +0300

    Added wishlist API

--- controllers/user.js
+++ controllers/user.js
@@ -122,3 +122,33 @@ exports.orders = async (req, res) => {
 
     await res.json(userOrders);
 };
+
+exports.addToWishList = async (req, res) => {
+    const { productId } = req.body;
+
+    const user = await User.findOneAndUpdate(
+        { email: req.user.email },
+        { $addToSet: { wishlist: productId }},
+        { new: true }
+    ).exec();
+
+    await res.json({ ok: true });
+};
+
+exports.wishlist = async (req, res) => {
+    const list = await User.findOne({ email: req.user.email })
+        .select("wishlist")
+        .populate("wishlist")
+        .exec()
+    await res.json(list);
+};
+
+exports.removeFromWishlist = async (req, res) => {
+    const { productId } = req.params;
+    const user = await User.findOneAndUpdate(
+        { email: req.user.email },
+        { $pull: { wishlist: productId }}
+    ).exec()
+
+    await res.json({ ok: true });
+};
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const { authCheck } = require('../middlewares/auth');
-const { userCart, getUserCart, emptyCart, saveAddress, applyCouponToUserCart, createOrder, orders } = require('../controllers/user');
+const {
+    userCart,
+    getUserCart,
+    emptyCart,
+    saveAddress,
+    applyCouponToUserCart,
+    createOrder,
+    orders,
+    addToWishList,
+    wishlist,
+    removeFromWishlist
+} = require('../controllers/user');
 
 const router = express.Router();
 
@@ -11,6 +22,8 @@ router.post('/user/address', authCheck, saveAddress);
 router.post('/user/cart/coupon', authCheck, applyCouponToUserCart);
 router.post('/user/order', authCheck, createOrder);
 router.get('/user/orders', authCheck, orders);
-
+router.post('/user/wishlist', authCheck, addToWishList);
+router.get('/user/wishlist', authCheck, wishlist);
+router.put('/user/wishlist/:productId', authCheck, removeFromWishlist);
 
 module.exports = router;
